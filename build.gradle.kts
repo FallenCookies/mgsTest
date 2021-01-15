cd1eceeffc6d990069f9300fd0b457a0fb848f2a
Vlad Zamaev <vlad_zg@bk.ru>
2021-01-15 17:55:45 +0300

    nice fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,6 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation ("org.webjars:webjars-locator-core")
 	implementation ("org.webjars:sockjs-client:1.0.2")
-	implementation ("org.webjars:stomp-websocket:2.3.3")
 	implementation ("org.webjars:bootstrap:3.3.7")
 	implementation ("org.webjars:jquery:3.1.1-1")
 	implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
